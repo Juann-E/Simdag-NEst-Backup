8aa60406a347cd20e998d764c382aa69582cacad
Rangga Prawiro Utomo <ranggautomo2003@gmail.com>
2025-09-15 12:01:03 +0700

    Coba Fix bug

--- Backend/src/modules/Kepokmas/nama-pasar/nama-pasar.controller.ts
+++ Backend/src/modules/Kepokmas/nama-pasar/nama-pasar.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, BadRequestException, UseGuards } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
